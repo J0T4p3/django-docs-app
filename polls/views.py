86a5a78e21c7da953bb78e041b4830fa8eb075a0
reversejp <jpslima@timbrasil.com.br>
2025-10-07 15:32:59 -0300

    Included tests for the models and views

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.db.models import F
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from polls.models import Choice, Question
@@ -12,12 +13,21 @@ class IndexView(generic.ListView):
     context_object_name = "latest_questions"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        # Filtra resultados em que a data de publicação já está no passado
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
@@ -26,7 +36,7 @@ class ResultsView(generic.DetailView):
 
 
 def votes(request, question_id):
-    question:Question = get_object_or_404(Question, pk=question_id)
+    question: Question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choices.get(pk=request.POST["choice"])  # type:ignore
     except (KeyError, Choice.DoesNotExist):
